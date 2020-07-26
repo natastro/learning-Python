b1dd974f12b0b7be9c8934fbcd294eda9a5034b5
Natalie Stroud <n.stroud@outlook.com>
2020-07-26 14:02:27 -0400

    added exercise from Scrimba and added to notes

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+msg= 'Welcome to Python 101: Strings'
+
+tyler= ("1 " + msg[0:7] + " ring to tyler")
+
+# Capitalizes first letter of each word
+print(tyler.title())
+
+tylerTitle= tyler.title()
+# print(tylerTitle)
+# Prints title string backwards
+print(tylerTitle[::-1])
